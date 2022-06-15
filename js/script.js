0089f2d728c4fb43d8e2f2073374c64b6f4d044f
tatsuji_ho <longer271271@gmail.com>
2022-06-15 14:40:56 +0900

    ファイルをbase64に変換

--- js/script.js
+++ js/script.js
@@ -7,5 +7,25 @@ function post() {
 
 function finput(input) {
     console.log(input);
-    console.log(input.files[0]);
+    const encodedFile = await base64Encode(input.files[0]);
+    console.log(encodedFile);
 }
+
+function base64Encode(...parts) {
+    return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const offset = reader.result.indexOf(",") + 1;
+            resolve(reader.result.slice(offset));
+        };
+        reader.readAsDataURL(new Blob(parts));
+    });
+}
+  
+function base64Decode(text, charset) {
+    return fetch(`data:text/plain;charset=${charset};base64,` + text).then(response => response.text());
+}
+  
+function base64DecodeAsBlob(text, type = "text/plain;charset=UTF-8") {
+    return fetch(`data:${type};base64,` + text).then(response => response.blob());
+} 
