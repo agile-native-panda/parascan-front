7607c099461100bf87fad6421709d12b99496fce
misaki-lon <s1911360@s.tsukuba.ac.jp>
2022-06-18 22:11:28 +0900

    base64に関する関数を削除

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 // jquery基本構文
 // $(“セレクタ”).メソッド(“パラメータ[引数]”);
 
+const form = document.getElementById("upload");
+const submitButton = document.getElementById("submit-button");
+
+
 function post() {
-    $("body").text("Hello World!");
-}
+    const formData = new FormData(document.getElementById("upload"));
+    const action = upload.getAttribute("action");
+    const options = {
+        method: 'POST',
+        body: formData,
+    }
 
-async function finput(input) {
-    console.log(input);
-    const encodedFile = await base64Encode(input.files[0]);
-    console.log(encodedFile);
+    fetch(action, options).then((e) => {
+        if(e.status === 200) {
+        alert("保存しました。")
+        return
+        }
+        alert("保存できませんでした。")
+    })
 }
-
-function base64Encode(...parts) {
-    return new Promise(resolve => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            const offset = reader.result.indexOf(",") + 1;
-            resolve(reader.result.slice(offset));
-        };
-        reader.readAsDataURL(new Blob(parts));
-    });
-}
-  
-function base64Decode(text, charset) {
-    return fetch(`data:text/plain;charset=${charset};base64,` + text).then(response => response.text());
-}
-  
-function base64DecodeAsBlob(text, type = "text/plain;charset=UTF-8") {
-    return fetch(`data:${type};base64,` + text).then(response => response.blob());
-} 
